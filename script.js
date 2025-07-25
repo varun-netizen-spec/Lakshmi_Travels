ec1d4c33551028baf74889a9c6d8ff1224e65943
varun-netizen-spec <varunvarma867@gmail.com>
2025-07-25 22:50:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for contacting Lakshmi Travels! We'll reach out soon.");
+});
